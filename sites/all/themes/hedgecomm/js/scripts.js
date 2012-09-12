fa861318f89982087d2df5ed4f6b4575b9f2cf42
Gerrit Vos <gevo27@gmail.com>
2012-09-12 13:54:18 +0200

    Style and Script changes

--- sites/all/themes/hedgecomm/js/scripts.js
+++ sites/all/themes/hedgecomm/js/scripts.js
@@ -13,4 +13,17 @@ jQuery(document).ready(function($) {
 	  },
 	  pagination : '#pager'
 	});
+	
+	
+	// equal heights for content and sidebar 2 (non front)
+	if ($("body").hasClass('not-front')) {
+		var contentHeight = $('#region-content').height();
+		var sidebarHeight = $('#region-sidebar-second').height();
+		if (contentHeight < sidebarHeight) {
+		  $('#region-content .region-inner').css('height', sidebarHeight);
+		} else {
+		  $('#region-sidebar-second .region-inner').css('height', contentHeight);		
+		}
+	}
+	
 });
